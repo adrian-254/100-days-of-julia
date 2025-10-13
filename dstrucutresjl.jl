f50b8d0b296cafd2ad68bea8aa942b8acaa138f1
adrian-254 <148047071+adrian-254@users.noreply.github.com>
2025-10-13 19:16:34 +0300

    Add files via upload

---
+++ dstrucutresjl.jl
@@ -0,0 +1,7 @@
+#Julia Mathematical D.Sturctures
+
+#1 Matrix and matrices
+using LinearAlgebra
+
+matrix1 = [1 2 3;4 5 6 ;7 8 9;-6 7 0]
+println(matrix1)
---
+++ randomno.jl
@@ -0,0 +1,3 @@
+#Generating random array numbers
+a = randn(4,2)
+println(a)

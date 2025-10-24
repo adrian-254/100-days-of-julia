f80d8101529a0f6e3153e25774733e99fa6b2344
adrian-254 <148047071+adrian-254@users.noreply.github.com>
2025-10-24 07:32:09 +0300

    Update fibonacci.jl

--- fibonacci.jl
+++ fibonacci.jl
@@ -1,5 +1,6 @@
-#Fibonacci in julia
+#Fibonacci in julia, cretaing fibonnacci sequence using recursion and ternary operator
 f(n) = n < 2 ? 1 : f(n - 1) + f(n - 2)
 for i = 0:10 
     println("(f($i) = $(f(i)))")
+
 end

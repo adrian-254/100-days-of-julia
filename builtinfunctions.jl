24991ad43f5cee13f04e817b2618ff31140f4189
adrian-254 <148047071+adrian-254@users.noreply.github.com>
2025-11-03 20:32:28 +0300

    Add files via upload
    Here i'll add all built in functions that i'm learning daily

---
+++ builtinfunctions.jl
@@ -0,0 +1,8 @@
+# implementation of FACTORIAL FUNCTION...it multiplies all positive integers up to n
+println("Built-in factorial(1): ", factorial(1))
+println("Built-in factorial(2): ", factorial(2))
+println("Built-in factorial(3): ", factorial(3))
+println("Built-in factorial(4): ", factorial(4))
+println("Built-in factorial(5): ", factorial(5))
+println("Built-in factorial(6): ", factorial(6)) #and so on
+
